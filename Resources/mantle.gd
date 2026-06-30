5b62f549e14a488d3848a9240642958f28b434eb
All-Chronical <175769506+All-Chronical@users.noreply.github.com>
2026-06-29 21:02:08 -0400

    feat: Rig attributes and base color

--- Resources/mantle.gd
+++ Resources/mantle.gd
@@ -2,4 +2,6 @@ extends Resource
 class_name Mantle
 
 @export var rigType: int = 0
+@export var baseColor: Color = Color.BLACK
+@export var rigNote: String = ""
 @export var notes: PackedStringArray
